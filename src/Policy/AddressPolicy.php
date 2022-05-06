814e2ec54c4e9c0112607da8eb8d2b46d930a32d
aleksandra-rapala <aleksandra.rapala18@gmail.com>
2022-05-06 02:32:37 +0200

    Added

---
+++ src/Policy/AddressPolicy.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Policy;
+
+use App\Model\Entity\Address;
+use Authorization\IdentityInterface;
+
+/**
+ * Address policy
+ */
+class AddressPolicy
+{
+    /**
+     * Check if $user can add Address
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Address $address
+     * @return bool
+     */
+    public function canAdd(IdentityInterface $user, Address $address)
+    {
+        return true;
+    }
+
+    /**
+     * Check if $user can edit Address
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Address $address
+     * @return bool
+     */
+    public function canEdit(IdentityInterface $user, Address $address)
+    {
+    }
+
+    /**
+     * Check if $user can delete Address
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Address $address
+     * @return bool
+     */
+    public function canDelete(IdentityInterface $user, Address $address)
+    {
+    }
+
+    /**
+     * Check if $user can view Address
+     *
+     * @param \Authorization\IdentityInterface $user The user.
+     * @param \App\Model\Entity\Address $address
+     * @return bool
+     */
+    public function canView(IdentityInterface $user, Address $address)
+    {
+    }
+}
