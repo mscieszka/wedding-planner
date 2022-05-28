68760719e47fe6f9bea82ff436b5a6245b98b77e
Sparkvove <57872726+Sparkvove@users.noreply.github.com>
2022-05-28 11:57:51 +0200

    edited view of a offers of a type(deleted most useless items, added link at name to redirect)

--- templates/Categories/view.php
+++ templates/Categories/view.php
@@ -15,53 +15,25 @@
     <div class="column-responsive column-80">
         <div class="categories view content">
             <h3><?= h($category->name) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Name') ?></th>
-                    <td><?= h($category->name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($category->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($category->created) ?></td>
-                </tr>
-            </table>
             <div class="related">
                 <h4><?= __('Related Offers') ?></h4>
                 <?php if (!empty($category->offers)) : ?>
                 <div class="table-responsive">
                     <table>
                         <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('User Id') ?></th>
-                            <th><?= __('Category Id') ?></th>
-                            <th><?= __('Address Id') ?></th>
                             <th><?= __('Name') ?></th>
                             <th><?= __('Price') ?></th>
                             <th><?= __('Description') ?></th>
-                            <th><?= __('Advance Payment') ?></th>
-                            <th><?= __('Website') ?></th>
-                            <th><?= __('Created') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
                         </tr>
                         <?php foreach ($category->offers as $offers) : ?>
                         <tr>
-                            <td><?= h($offers->id) ?></td>
-                            <td><?= h($offers->user_id) ?></td>
-                            <td><?= h($offers->category_id) ?></td>
-                            <td><?= h($offers->address_id) ?></td>
-                            <td><?= h($offers->name) ?></td>
+                            <td><?= $this->Html->link(__($offers->name), ['controller' => 'Offers', 'action' => 'view', $offers->id]) ?></td>
                             <td><?= h($offers->price) ?></td>
                             <td><?= h($offers->description) ?></td>
-                            <td><?= h($offers->advance_payment) ?></td>
-                            <td><?= h($offers->website) ?></td>
-                            <td><?= h($offers->created) ?></td>
+
+
+                            <?php if($account_type_id == 2): ?>
                             <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'Offers', 'action' => 'view', $offers->id]) ?>
-                                <?php if($account_type_id == 2): ?>
                                     <?php if (($offers->user_id) == $id_user_log):?>
                                         <?= $this->Html->link(__('Edit'), ['controller' => 'Offers', 'action' => 'edit', $offers->id]) ?>
                                         <?= $this->Form->postLink(__('Delete'), ['controller' => 'Offers', 'action' => 'delete', $offers->id], ['confirm' => __('Are you sure you want to delete # {0}?', $offers->id)]) ?>
