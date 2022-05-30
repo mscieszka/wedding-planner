8b529cec7cb4665eb7d448f3678728a0274fee6f
efryd <efrydzewski@gmail.com>
2022-05-31 00:01:40 +0200

    adjust header, styling provider profile

--- templates/Users/view.php
+++ templates/Users/view.php
@@ -4,7 +4,7 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
-<?= $this->Html->css('viewUser') ?>
+<?= $this->Html->css(['viewUser', 'miligram.min', 'normalize.min', 'viewProvider']) ?>
 <div class="row">
     <!--<aside class="column">
         <div class="side-nav">
@@ -18,8 +18,36 @@
     <nav>
         <?= $this->Html->image('logo.svg', ['alt' => 'Wedding Planner']); ?>
     </nav>
-    <div class="column-responsive column-80">
-        <div>Tutaj będą informacje profilowe zrobione przez Filipa</div>
+    <div class="column-responsive column-80 " >
+        <div class="provider_container">
+            <div class="provider_image">
+                <a>
+                <img src="/img/userProfileImage/userProfileImage3.jpg" alt="Owner profile image" class="ownerimg">
+                </a>
+            </div>
+            <div class="provider_info">
+            <div class="provider_name">
+                 <p class="property_name"><?= __('Name:   ') ?></p>
+                    <p><?= h($user->name) ?></p>
+                    <p><?= h($user->surname) ?></p>
+            </div>
+            <div class="provider_contact">
+                <div class="provider_contact_detail">
+                    <p class="property_name"><?= __('Phone Number:   ') ?></p>
+                    <p><?= h($user->phone_number) ?></p>
+                </div>
+                <div class="provider_contact_detail">
+
+                    <p class="property_name"><?= __('Email:') ?></p>
+                    <p><?= h($user->email) ?></p>
+                </div>
+            </div>
+            </div>
+                <div class="provider_edit">
+                    <?= $this->Html->link(__('Edit User'), ['action' => 'edit', $user->id], ['class' => 'side-nav-item button float-right']) ?>
+                </div>
+
+        </div>
         <div class="bookmarks_wrapper">
             <div class="current_bookmarks">Zarządzanie ofertami</div>
             <div><?= $this->Html->link(__('Otrzymane oceny'), ['action' => '']) ?></div>
