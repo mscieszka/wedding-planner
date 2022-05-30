071eafb69da273530fc606055bd607c5f761a2f9
MarcinScieszka <79609951+MarcinScieszka@users.noreply.github.com>
2022-05-30 16:27:47 +0200

    fix main button style throughout application

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -84,12 +84,12 @@ endif;
         </div>
     </div>
     <?php if($account_type_id == 1): ?>
-        <?= $this->Html->link(__('My saved offers'), ['controller' => 'Offers', 'action' => 'index', 2],  ['class' => 'button', 'id' => 'homepage-login']) ?>
+        <?= $this->Html->link(__('My saved offers'), ['controller' => 'Offers', 'action' => 'index', 2],  ['class' => 'button']) ?>
     <?php endif; ?>
     <?php if($account_type_id == 2): ?>
-        <?= $this->Html->link(__('My Offer'), ['controller' => 'Offers', 'action' => 'index',1],['class' => 'button', 'id' => 'homepage-login']) ?>
+        <?= $this->Html->link(__('My Offer'), ['controller' => 'Offers', 'action' => 'index',1],['class' => 'button']) ?>
     <?php endif; ?>
-    <?= $this->Html->link(__('Profil'), ['controller' => 'Users', 'action' => 'profile'], ['class' => 'button', 'id' => 'homepage-login']) ?>
+    <?= $this->Html->link(__('Profil'), ['controller' => 'Users', 'action' => 'profile'], ['class' => 'button']) ?>
 </header>
 <?= $this->Flash->render() ?>
     <div class="content_wrapper_homepage">
