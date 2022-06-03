1530bb42f04b7ee6b43055e165d8774ef645c075
michalrudy9 <michallrudy9@gmail.com>
2022-06-03 21:10:50 +0200

    Zakladka oceny - wystylizowanie u providera i klienta

--- templates/Users/ocenyprofil.php
+++ templates/Users/ocenyprofil.php
@@ -56,20 +56,19 @@
                                         <?= $this->Html->image('userProfileImage/userProfileImage1.jpg', ['alt' => 'User profile image', 'class' => 'userimg'])  ?>
                                     </a>
                                     <div class="upper-box_provider">
-                                        <h2><?= $rating->has('user') ? $this->Html->link($rating->user->name, ['controller' => 'Users', 'action' => 'profile', 1, $rating->user->id]) : '' ?></h2>
-                                        <h2><?= $rating->has('offer') ? $this->Html->link($rating->offer->name, ['controller' => 'Offers', 'action' => 'view', $rating->offer->id]) : '' ?></h2>
-                                        <blockquote>
+                                        <span style="width: 18%"><?= $rating->has('user') ? $this->Html->link($rating->user->name, ['controller' => 'Users', 'action' => 'profile', 1, $rating->user->id]) : '' ?></span>
+                                        <p style="width: 5%">do</p>
+                                        <span style="width: 15%"><?= $rating->has('offer') ? $this->Html->link($rating->offer->name, ['controller' => 'Offers', 'action' => 'view', $rating->offer->id]) : '' ?></span>
+                                        <blockquote style="width: 50%">
                                             <?= $this->Text->autoParagraph(h($rating->description)); ?>
                                         </blockquote>
+                                        <?= h($rating->opinion_date) ?>
                                     </div>
-                                    <div class="opinion-content_provider">
-                                        <h4><?= h($rating->opinion_date) ?></h4>
+                                    <div class="opinion-remove">
+                                        <?php if ($rating->user_id == $id_user_log) : ?>
+                                            <td class="offer-name"><?= $this->Form->postLink(__('Usuń komentarz'), ['controller' => 'Ratings', 'action' => 'delete', $rating->id], ['confirm' => __('Czy na pewno chcesz usunąć ten komentarz?')]) ?></td>
+                                        <?php endif; ?>
                                     </div>
-
-                                    <?php if ($rating->user_id == $id_user_log) : ?>
-                                        <td class="offer-name"><?= $this->Form->postLink(__('Usuń komentarz'), ['controller' => 'Ratings', 'action' => 'delete', $rating->id], ['confirm' => __('Czy na pewno chcesz usunąć ten komentarz?')]) ?></td>
-                                    <?php endif; ?>
-
                                 </div>
                             <?php endforeach; ?>
                         <?php endif; ?>
@@ -84,17 +83,16 @@
                                             <?= $this->Html->image('userProfileImage/userProfileImage1.jpg', ['alt' => 'User profile image', 'class' => 'userimg'])  ?>
                                         </a>
                                         <div class="upper-box_provider">
-                                            <h2><?= $rating->has('user') ? $this->Html->link($rating->user->name, ['controller' => 'Users', 'action' => 'profile', 1, $rating->user->id]) : '' ?></h2>
-                                            <h2><?= $rating->has('offer') ? $this->Html->link($rating->offer->name, ['controller' => 'Offers', 'action' => 'view', $rating->offer->id]) : '' ?></h2>
-                                            <blockquote>
+                                            <span style="width: 18%"><?= $rating->has('user') ? $this->Html->link($rating->user->name, ['controller' => 'Users', 'action' => 'profile', 1, $rating->user->id]) : '' ?></span>
+                                            <p style="width: 5%">do</p>
+                                            <span style="width: 15%"><?= $rating->has('offer') ? $this->Html->link($rating->offer->name, ['controller' => 'Offers', 'action' => 'view', $rating->offer->id]) : '' ?></span>
+                                            <blockquote style="width: 50%">
                                                 <?= $this->Text->autoParagraph(h($rating->description)); ?>
                                             </blockquote>
-                                        </div>
-                                        <div class="opinion-content_provider">
-                                            <h4><?= h($rating->opinion_date) ?></h4>
+                                            <?= h($rating->opinion_date) ?>
                                         </div>
 
-                                        <div class="offer_container_edit_button">
+                                        <div class="opinion-remove">
                                             <?php if ($rating->user_id == $id_user_log) : ?>
                                                 <td class="offer-name"><?= $this->Form->postLink(__('Usuń opinię'), ['controller' => 'Ratings', 'action' => 'delete', $rating->id], ['confirm' => __('Czy na pewno chcesz usunąć opinię?')]) ?></td>
                                             <?php endif; ?>
