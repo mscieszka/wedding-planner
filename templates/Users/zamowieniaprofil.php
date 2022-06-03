d0581b2ad0cf80f2da23d5464289f540f35e0aea
michalrudy9 <michallrudy9@gmail.com>
2022-06-03 19:59:06 +0200

    Provider - wystylizowanie zakladka zamowienia

--- templates/Users/zamowieniaprofil.php
+++ templates/Users/zamowieniaprofil.php
@@ -73,6 +73,9 @@
                     <tr>
                         <th>Data złożenia zamówienia</th>
                         <th>Data wykonania usługi</th>
+                        <?php if($user->account_type_id == 2): ?>
+                            <th>Użytkownik</th>
+                        <?php endif; ?>
                         <th>Oferta</th>
                         <th>Status</th>
                     </tr>
@@ -101,7 +104,7 @@
                                                     <?php endif; ?>
 
                                                     <?php if($booking->booking_date >= date("Y-m-d")) : ?>
-                                                        <td><p>Zamowienie zakonczone </p></td>
+                                                        <td><p>Zamowienie zakonczone</p></td>
                                                     <?php endif; ?>
                                                 <?php endif; ?>
                                             <!--</div>-->
@@ -114,7 +117,7 @@
                     <?php endforeach; ?>
 
                 <?php endif; ?>
-                </table>
+
 
 
 
@@ -123,46 +126,41 @@
 
 
                 <?php if($user->account_type_id == 2): ?>
-
                     <?php foreach ($bookings as $booking) : ?>
+                        <?php if(!(in_array($booking->offer_id, $his_offers))): continue; ?>
+                        <?php endif; ?>
+                        <tr>
+                            <!--<div class="profile-order-container">-->
+                                <!--<div class="profile-order-property">-->
+                                    <!--<p class="property_name"><?= __('Data zlozenia zamowienia:   ') ?></p>-->
+                                    <td><?= h($booking->created) ?></td>
+                                    <!--<p class="property_name"><?= __('Data wykonania uslugi:   ') ?></p>-->
+                                    <td><?= h($booking->booking_date) ?></td>
+                                <!--</div>-->
+                                <!--<div class="profile-order-property">-->
+                                    <td><?= $booking->has('user') ? $this->Html->link($booking->user->name, ['controller' => 'Offers', 'action' => 'view',  $booking->offer->id]) : '' ?></td>
+                                    <td><?= $booking->has('offer') ? $this->Html->link($booking->offer->name, ['controller' => 'Offers', 'action' => 'view',  $booking->offer->id]) : '' ?></td>
+                                <!--</div>-->
+                                <!--<div class="profile-order-property">-->
+                                    <!--<td class="actions">-->
+                                        <!--<div class="profile-order-actions">-->
+                                            <?php if($booking->booking_date < date("Y-m-d")) : ?>
+                                                <td><p>Zamowienie w trakcie realizacji</p></td>
+                                            <?php endif; ?>
 
-                            <?php if(!(in_array($booking->offer_id, $his_offers))): continue; ?>
-                            <?php endif; ?>
-
-
-                            <tr>
-                                <div class="profile-order-container">
-                                    <div class="profile-order-property">
-                                        <p class="property_name"><?= __('Data zlozenia zamowienia:   ') ?></p>
-                                        <td><?= h($booking->created) ?></td>
-                                        <p class="property_name"><?= __('Data wykonania uslugi:   ') ?></p>
-                                        <td><?= h($booking->booking_date) ?></td>
-                                    </div>
-                                    <div class="profile-order-property">
-                                        <h2><?= $booking->has('user') ? $this->Html->link($booking->user->name, ['controller' => 'Offers', 'action' => 'view',  $booking->offer->id]) : '' ?></h2>
-                                        <h2><?= $booking->has('offer') ? $this->Html->link($booking->offer->name, ['controller' => 'Offers', 'action' => 'view',  $booking->offer->id]) : '' ?></h2>
-                                    </div>
-                                    <div class="profile-order-property">
-                                        <td class="actions">
-                                            <div class="profile-order-actions">
-
-                                                    <?php if($booking->booking_date < date("Y-m-d")) : ?>
-                                                        <p>Zamowienie w trakcie realizacji. </p>
-                                                    <?php endif; ?>
-                                                    <?php if($booking->booking_date >= date("Y-m-d")) : ?>
-                                                        <p>Zamowienie zakonczone </p>
-                                                    <?php endif; ?>
-
-                                            </div>
-                                        </td>
-                                    </div>
-                                </div>
-                            </tr>
+                                            <?php if($booking->booking_date >= date("Y-m-d")) : ?>
+                                                <td><p>Zamowienie zakonczone</p></td>
+                                            <?php endif; ?>
+                                        <!--</div>-->
+                                    <!--</td>-->
+                                <!--</div>-->
+                            <!--</div>-->
+                        </tr>
 
                     <?php endforeach; ?>
 
                 <?php endif; ?>
-
+                </table>
 
                                           <!--  <?= $this->Html->link(__('Edit'), ['controller' => 'SavedUserBookings', 'action' => 'edit', $booking->id], ['class' => 'button profile-order-btn']) ?> -->
                                            <!-- <?= $this->Form->postLink(__('Delete'), ['controller' => 'SavedUserBookings', 'action' => 'delete', $booking->id], ['confirm' => __('Are you sure you want to delete # {0}?', $booking->id), 'class' => 'button profile-order-btn profile-order-btn-red']) ?> -->
