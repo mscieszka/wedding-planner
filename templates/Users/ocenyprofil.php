c93b5aba104ec2e38164b012442c8d72e674dcac
michalrudy9 <michallrudy9@gmail.com>
2022-06-04 19:54:29 +0200

    Poprawki #priorytet0 #users

--- templates/Users/ocenyprofil.php
+++ templates/Users/ocenyprofil.php
@@ -36,18 +36,18 @@ use Cake\Filesystem\Folder;
 
         <div class="users view content">
             <div class="users view content_header">
-                <div class="users view content_header_first">Ocena</div>
+                <!--<div class="users view content_header_first">Ocena</div>-->
                 <div class="users view content_header_second" style="width: 75%; padding-left: 1em;">Opinie</div>
             </div>
             <div class="container_box">
-                <div class="users view content_wrapper" style="display: flex; justify-content: space-around">
+                <!--<div class="users view content_wrapper" style="display: flex; justify-content: space-around">
                     <div class="users view content_wrapper_box">
                         <div>
                             <div style="color: #000; font-size: 2em; height: 50%;">5.0</div>
                             <div style="color: gold; font-size: 2em; height: 50%;">*****</div>
                         </div>
                     </div>
-                </div>
+                </div>-->
                 <div class="opinion-box-wrapper">
                     <?php if ($user->account_type_id == 2) : ?>
                         <?php if (!empty($ratings)) : ?>
@@ -92,7 +92,8 @@ use Cake\Filesystem\Folder;
                                         <span style="width: 18%"><?= $rating->has('user') ? $this->Html->link($rating->user->name, ['controller' => 'Users', 'action' => 'profile', 1, $rating->user->id]) : '' ?></span>
                                         <p style="width: 5%">do</p>
                                         <span style="width: 15%"><?= $rating->has('offer') ? $this->Html->link($rating->offer->name, ['controller' => 'Offers', 'action' => 'view', $rating->offer->id]) : '' ?></span>
-                                        <blockquote style="width: 50%">
+                                        <!--Tutaj trzeba przesłać ocenę jaka została przyznana, cyfra 5 zawsze zostaje--><span style="width: 6%">4/5</span>
+                                        <blockquote style="width: 55%">
                                             <?= $this->Text->autoParagraph(h($rating->description)); ?>
                                         </blockquote>
                                         <?= h($rating->opinion_date) ?>
@@ -112,7 +113,7 @@ use Cake\Filesystem\Folder;
                             <?php foreach ($ratings as $rating) : ?>
                                 <?php if ($rating->user_id == $user->id) :  ?>
                                     <div class="opinion-box_provider">
-                                        <a class="user-img_provider">
+                                        <!--<a class="user-img_provider">
                                             <?php
                                             $path = WWW_ROOT.'img'.DS.'userProfileImage'.DS. $rating->user_id;
                                             if(!file_exists($path)) {
@@ -141,12 +142,13 @@ use Cake\Filesystem\Folder;
                                                     <?php break; ?>
                                                 <?php endforeach; ?>
                                             <?php endif; ?>
-                                        </a>
+                                        </a>-->
                                         <div class="upper-box_provider">
-                                            <span style="width: 18%"><?= $rating->has('user') ? $this->Html->link($rating->user->name, ['controller' => 'Users', 'action' => 'profile', 1, $rating->user->id]) : '' ?></span>
+                                            <span style="width: 13%"><?= $rating->has('user') ? $this->Html->link($rating->user->name, ['controller' => 'Users', 'action' => 'profile', 1, $rating->user->id]) : '' ?></span>
                                             <p style="width: 5%">do</p>
                                             <span style="width: 15%"><?= $rating->has('offer') ? $this->Html->link($rating->offer->name, ['controller' => 'Offers', 'action' => 'view', $rating->offer->id]) : '' ?></span>
-                                            <blockquote style="width: 50%">
+<!--Tutaj trzeba przesłać ocenę jaka została przyznana, cyfra 5 zawsze zostaje--><span style="width: 6%">4/5</span>
+                                            <blockquote style="width: 59%">
                                                 <?= $this->Text->autoParagraph(h($rating->description)); ?>
                                             </blockquote>
                                             <?= h($rating->opinion_date) ?>
