c8b79775dd1e14678adad31ade386e6f66a58627
MarcinScieszka <79609951+MarcinScieszka@users.noreply.github.com>
2022-06-04 21:19:43 +0200

    update content, style and translate user profile edit page

--- templates/Users/edit.php
+++ templates/Users/edit.php
@@ -8,39 +8,32 @@
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $user->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('Change Password'), ['action' => 'changePassword'], ['class' => 'side-nav-item']) ?>
+            <?= $this->Html->link(__('Zmiana hasła'), ['action' => 'changePassword'], ['class' => 'side-nav-item']) ?>
         </div>
     </aside>
     <div class="column-responsive column-80">
         <div class="users form content">
             <?= $this->Form->create($user,  ['type' => 'file']) ?>
-
             <fieldset>
-                <legend><?= __('Edit User') ?></legend>
                 <?php
                     echo $this->Form->control('email');
-                    echo $this->Form->control('password');
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('surname');
-                    echo $this->Form->control('phone_number');
-                    echo $this->Form->control('enabled');
-                    echo $this->Form->control('confirmed_email');
-                    echo $this->Form->control('company_name');
-                    echo $this->Form->control('krs');
-                    echo $this->Form->control('nip');
-                    echo $this->Form->control('regon');
-                    echo $this->Form->control('account_type_id', ['options' => $accountTypes]);
-                    echo $this->Form->control('attachment[]', ['required' => false, 'type' => 'file','multiple'=>true, 'label' => '']);
+                    echo $this->Form->control('name', ['label' => __('Imię')]);
+                    echo $this->Form->control('surname', ['label' => __('Nazwisko')]);
+                    echo $this->Form->control('phone_number', ['label' => __('Numer telefonu')]);
+                    if ($user->account_type_id == 2) {
+                        echo $this->Form->control('company_name');
+                        echo $this->Form->control('krs');
+                        echo $this->Form->control('nip');
+                    }
+                    echo $this->Form->control('attachment[]', [
+                        'required' => false,
+                        'type' => 'file',
+                        'multiple'=> true,
+                        'label' => 'Zdjęcie profilowe'
+                    ]);
                 ?>
             </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->button(__('Potwierdź edycję')) ?>
             <?= $this->Form->end() ?>
         </div>
     </div>
