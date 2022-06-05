99c402b3db51e147d2d2a4154a43618324378c44
aleksandrarudy <79640332+aleksandrarudy@users.noreply.github.com>
2022-06-05 12:47:16 +0200

    styling provinces list

--- templates/Provinces/index.php
+++ templates/Provinces/index.php
@@ -6,22 +6,14 @@
  */
 ?>
 <?= $this->Html->css('provinces') ?>
-<div class="provinces index content">
+<div class="provinces-index-content">
     <h3><?= __('Wyświetl oferty wg województwa') ?></h3>
     <div class="table-responsive">
-        <table>
-            <thead>
-            <tr>
-                <th><?= $this->Paginator->sort('name', 'Województwo', ['direction' => 'asc']) ?></th>
-            </tr>
-            </thead>
-            <tbody>
+            <h4><?= $this->Paginator->sort('name', 'Województwo', ['direction' => 'asc']) ?></h4>
+            <div class="single-province">
             <?php foreach ($provinces as $province) : ?>
-                <tr>
-                    <td><?= $this->Html->link(h($province->name), ['action' => 'view', $province->id]) ?></td>
-                </tr>
+                <h5><?= $this->Html->link(h($province->name), ['action' => 'view', $province->id]) ?></h5>
             <?php endforeach; ?>
-            </tbody>
-        </table>
+            </div>
     </div>
 </div>
